163a090aa4f3980c93d56fab61c748f96e0540a0
Magshimim <ac8274@bs.amalnet.k12.il>
2025-01-14 20:42:12 +0200

    finished arm

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,103 +4,60 @@
 
 package frc.robot;
 
-import com.pathplanner.lib.auto.NamedCommands;
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.swervedrive.drivebase.AbsoluteDriveAdv;
-import frc.robot.subsystems.swervedrive.SwerveSubsystem;
-import java.io.File;
-import swervelib.SwerveInputStream;
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
 public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-    final CommandXboxController driverXbox = new CommandXboxController(0);
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
-    private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
-            "swerve"));
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+  }
 
-    public RobotContainer() {
-        // Configure the trigger bindings
-        configureBindings();
-        DriverStation.silenceJoystickConnectionWarning(true);
-        NamedCommands.registerCommand("test", Commands.print("I EXIST"));
-    }
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    private void configureBindings() {
-        // (Condition) ? Return-On-True : Return-on-False
-        drivebase.setDefaultCommand(
-                !RobotBase.isSimulation() ? driveFieldOrientedAnglularVelocity : driveFieldOrientedDirectAngleSim);
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  }
 
-        driverXbox.start().onTrue((Commands.runOnce(drivebase::zeroGyro)));
-        driverXbox.b().whileTrue(
-                drivebase.driveToPose(
-                        new Pose2d(new Translation2d(4, 4), Rotation2d.fromDegrees(0))));
-
-    }
-
-    public Command getAutonomousCommand() {
-        // An example command will be run in autonomous
-        return drivebase.getAutonomousCommand("New Auto");
-    }
-
-    public void setDriveMode() {
-        configureBindings();
-    }
-
-    public void setMotorBrake(boolean brake) {
-        drivebase.setMotorBrake(brake);
-    }
-
-    AbsoluteDriveAdv closedAbsoluteDriveAdv = new AbsoluteDriveAdv(drivebase,
-            () -> -MathUtil.applyDeadband(driverXbox.getLeftY(),
-                    OperatorConstants.LEFT_Y_DEADBAND),
-            () -> -MathUtil.applyDeadband(driverXbox.getLeftX(),
-                    OperatorConstants.DEADBAND),
-            () -> -MathUtil.applyDeadband(driverXbox.getRightX(),
-                    OperatorConstants.RIGHT_X_DEADBAND),
-            driverXbox.getHID()::getYButtonPressed,
-            driverXbox.getHID()::getAButtonPressed,
-            driverXbox.getHID()::getXButtonPressed,
-            driverXbox.getHID()::getBButtonPressed);
-
-    SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
-            () -> driverXbox.getLeftY() * -1,
-            () -> -1)
-            .withControllerRotationAxis(driverXbox::getRightX)
-            .deadband(OperatorConstants.DEADBAND)
-            .scaleTranslation(0.8)
-            .allianceRelativeControl(true);
-
-    Command driveFieldOrientedAnglularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
-
-    SwerveInputStream driveAngularVelocitySim = SwerveInputStream.of(drivebase.getSwerveDrive(),
-            () -> -driverXbox.getLeftY(),
-            () -> -driverXbox.getLeftX())
-            .withControllerRotationAxis(() -> driverXbox.getRawAxis(2))
-            .deadband(OperatorConstants.DEADBAND)
-            .scaleTranslation(0.8)
-            .allianceRelativeControl(true);
-
-    SwerveInputStream driveDirectAngleSim = driveAngularVelocitySim.copy()
-            .withControllerHeadingAxis(() -> Math.sin(
-                    driverXbox.getRawAxis(
-                            2) * Math.PI)
-                    * (Math.PI * 2),
-                    () -> Math.cos(
-                            driverXbox.getRawAxis(
-                                    2) * Math.PI)
-                            *
-                            (Math.PI * 2))
-            .headingWhile(true);
-
-    Command driveFieldOrientedDirectAngleSim = drivebase.driveFieldOriented(driveDirectAngleSim);
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_exampleSubsystem);
+  }
 }
---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,128 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import org.opencv.core.Mat;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import frc.robot.Constants;
+
+public class Arm extends SubsystemBase {
+  private SparkFlex motor;
+  private SparkFlexConfig motorConfig;
+  private SparkClosedLoopController closedLoopController;
+  private RelativeEncoder encoder;
+  private static Arm instance=null;
+  /** Creates a new Arm. */
+  public Arm() {
+    this.motor = new SparkFlex(Constants.ARM_MOTOR_ID, MotorType.kBrushless); // the type is in need to be changed
+    this.closedLoopController = this.motor.getClosedLoopController();
+    this.encoder = this.motor.getEncoder();
+
+    /*
+     * Create a new SPARK MAX configuration object. This will store the
+     * configuration parameters for the SPARK MAX that we will set below.
+     */
+    motorConfig = new SparkFlexConfig();
+
+    // configure encoder to specific conversion factor.
+    this.motorConfig.encoder.positionConversionFactor(Constants.ARM_POSITION_CONVERTION_FACTOR);
+    
+    //set the motor to break mode so that the motor wont move.
+    this.motorConfig.idleMode(IdleMode.kBrake);
+
+    //configuring the encoders close loop 
+    this.motorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+    .p(Constants.ARM_P)
+    .i(Constants.ARM_I)
+    .d(Constants.ARM_D)
+    .outputRange(Constants.MIN_OUTPUT_RANGE, Constants.MAX_OUTPUT_RANGE);
+
+    //set max acceleration and velocity.
+    this.motorConfig.closedLoop.maxMotion
+    .maxVelocity(Constants.MAX_ARM_VELOCITY)
+    .maxAcceleration(Constants.MAX_ARM_ACCELERATION)
+    .allowedClosedLoopError(1);
+
+    // configuring the motor to save the settings.
+    this.motor.configure(this.motorConfig,ResetMode.kResetSafeParameters,PersistMode.kPersistParameters);
+
+    // in question if the encoder position in need of setting to 0.
+    this.encoder.setPosition(this.motor.getAbsoluteEncoder().getPosition());
+  }
+
+  /**
+   * Returns an instance of the class.
+   * @return an instance of the class
+   */
+
+  public static Arm getInstance()
+  {
+    if(instance == null)
+    {
+      instance = new Arm();
+    }
+    return instance;
+  }
+
+  /***
+   * The function moves the motor to the wanted angle
+   * 
+   * @param angle - the angle to move the motor to
+   */
+  public void setAngle(double angle)
+  {
+    //sets the arm to a target angle
+    this.closedLoopController.setReference(angle,  ControlType.kMAXMotionPositionControl,ClosedLoopSlot.kSlot0);
+  }
+
+  /***
+   * The function checks if the motor has reached the wanted angle
+   * 
+   * @param angle - The angle we want the motor to move to
+   * @return if the motor has reached the wanted angle
+   */
+  public boolean isAtAngle(double angle)
+  {
+    return encoder.getPosition() <= angle + Constants.ARM_TOLARANCE &&
+    encoder.getPosition() >= angle - Constants.ARM_TOLARANCE;
+  }
+
+
+  public double getCurrentAngle_In_Rads()
+  {
+    return this.encoder.getPosition();
+  }
+
+  public void setVoltage(double voltage)
+  {
+    this.motor.setVoltage(voltage);
+  }
+
+  public void stopMotor()
+  {
+    this.motor.stopMotor();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
