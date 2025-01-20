bd734cf35106978f914ce5024fb0ae11e1f92853
Magshimim <ac8274@bs.amalnet.k12.il>
2025-01-20 21:30:14 +0200

    added command to move manualy the algae collection and binded it to the circle

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,8 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.CollectAlgae;
+import frc.robot.commands.ManualAlgaeCollection;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
@@ -19,6 +21,7 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
+    m_driverController.circle().whileTrue(new ManualAlgaeCollection(Constants.ALGAE_INTAKE_TEST_VELOCITY));
   }
 
 
