8ab8e27bfe915520f8eb78365c9df000ff61333b
Ori Roitzaid <oriroitzaid@gmail.com>
2025-03-08 21:58:45 +0200

    Added height parameter to AutoDiagonal routine

--- src/main/java/frc/robot/commands/swervedrive/auto/AutoDiagonal.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/AutoDiagonal.java
@@ -4,7 +4,6 @@
 
 package frc.robot.commands.swervedrive.auto;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -12,19 +11,19 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.commands.AlignToReefTagRelative;
-import frc.robot.commands.MoveAlgaeArmToAngle;
+import frc.robot.commands.ElevatorDefaultCommand;
 import frc.robot.commands.MoveElevatorToPlace;
 import frc.robot.commands.ScoreCoral;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
-public class AutoDiagonalL2 extends SequentialCommandGroup {
-  public AutoDiagonalL2(SwerveSubsystem drivebase, boolean isRightScore) {
+public class AutoDiagonal extends SequentialCommandGroup {
+  public AutoDiagonal(SwerveSubsystem drivebase, boolean isRightScore, double elevatorHeight) {
         addCommands(
       new WaitCommand(0),
       new RunCommand(() -> drivebase.drive(new Translation2d(1.3,0), 0, false), drivebase).withTimeout(2.3),
       new InstantCommand(()->drivebase.drive(new Translation2d(0,0), 0,false)),
       new AlignToReefTagRelative(isRightScore, drivebase).withTimeout(4),
-      new MoveElevatorToPlace(Constants.L2_HEIGHT),
-      new ScoreCoral(0.1).withTimeout(2));
+      new MoveElevatorToPlace(elevatorHeight, Constants.ELEVATOR_POSITION_TOLERANCE).withTimeout(4),
+      new ElevatorDefaultCommand().alongWith(new ScoreCoral(1).withTimeout(2)).withTimeout(2));
   }
 }
