3fc224ce5577dcdf82ec58d74b3b2e2f8314a296
Ori Roitzaid <oriroitzaid@gmail.com>
2025-03-11 23:04:05 +0200

    Fixed AutoDiagonalL4 in theory

--- src/main/java/frc/robot/commands/swervedrive/auto/AutoDiagonalL4.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/AutoDiagonalL4.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
+import frc.robot.LimelightHelpers;
 import frc.robot.commands.AlignToReefTagRelative;
 import frc.robot.commands.ElevatorDefaultCommand;
 import frc.robot.commands.MoveElevatorToPlace;
@@ -20,12 +21,12 @@ public class AutoDiagonalL4 extends SequentialCommandGroup {
   public AutoDiagonalL4(SwerveSubsystem drivebase, boolean isRightScore) {
         addCommands(
       new WaitCommand(0),
-      new RunCommand(() -> drivebase.drive(new Translation2d(1.3,0), 0, false), drivebase).withTimeout(2.3),
+      new RunCommand(() -> drivebase.drive(new Translation2d(1.3,0), 0, false), drivebase).until(()-> LimelightHelpers.getBotPose_TargetSpace("")[2] <= 1)  ,
       new InstantCommand(()->drivebase.drive(new Translation2d(0,0), 0,false)),
       new AlignToReefTagRelative(isRightScore, drivebase).withTimeout(4),
       new MoveElevatorToPlace(Constants.L4_HEIGHT, Constants.ELEVATOR_POSITION_TOLERANCE).withTimeout(3),
-      new ElevatorDefaultCommand().alongWith(new ScoreCoral(0.5).withTimeout(2).
-      andThen(new RunCommand(() -> drivebase.drive(new Translation2d(-0.3,0), 0, false), drivebase).withTimeout(0.5))),
+      new ElevatorDefaultCommand().withTimeout(3).alongWith(new ScoreCoral(1, false).withTimeout(2).
+      andThen(new RunCommand(() -> drivebase.drive(new Translation2d(-0.3,0), 0, false), drivebase))).withTimeout(0.5),
       new MoveElevatorToPlace(Constants.CLOSED_HEIGHT, Constants.ELEVATOR_POSITION_TOLERANCE).withTimeout(3));
   }
 }
